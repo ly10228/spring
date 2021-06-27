419ce87a60fc1b3882cbe0e17f2135b2ccc9a5f9
ly10228 <601520157@qq.com>
2021-06-27 10:52:32 +0800

    Aware接口；帮我们装配Spring底层的一些组件

--- src/main/java/com/ly/springannotation/core/aware/UserAware.java
+++ src/main/java/com/ly/springannotation/core/aware/UserAware.java
@@ -4,19 +4,28 @@ import lombok.Data;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
 import org.springframework.stereotype.Component;
 
 /**
  * @author luoyong
  *  * @create 2021-06-26 7:52 下午
  *  * @last modify by [luoyong 2021-06-26 7:52 下午]
- * @Description: User
+ * @Description: UserAware  Aware接口；帮我们装配Spring底层的一些组件
  **/
 @Data
 @Component
-public class UserAware implements ApplicationContextAware {
+public class UserAware implements ApplicationContextAware, MessageSourceAware {
 
     private ApplicationContext applicationContext;
+
+    private MessageSource messageSource;
+
+    @Override
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource=messageSource ;
+    }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
